27ded68a601fdf62ea652a4ca3ac1fc1ad9bf8f9
Dawid Dopart <dawid.dopart@gmail.com>
2020-10-26 10:59:25 +0100

    properties of scan

--- Skaner/Skaner_kod/Skaner/Form1.Designer.cs
+++ Skaner/Skaner_kod/Skaner/Form1.Designer.cs
@@ -47,19 +47,19 @@
             // 
             // scanImage
             // 
-            this.scanImage.Location = new System.Drawing.Point(350, 28);
-            this.scanImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scanImage.Location = new System.Drawing.Point(467, 34);
+            this.scanImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.scanImage.Name = "scanImage";
-            this.scanImage.Size = new System.Drawing.Size(214, 308);
+            this.scanImage.Size = new System.Drawing.Size(285, 379);
             this.scanImage.TabIndex = 0;
             this.scanImage.TabStop = false;
             // 
             // scanButton
             // 
-            this.scanButton.Location = new System.Drawing.Point(42, 293);
-            this.scanButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.scanButton.Location = new System.Drawing.Point(56, 361);
+            this.scanButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.scanButton.Name = "scanButton";
-            this.scanButton.Size = new System.Drawing.Size(83, 43);
+            this.scanButton.Size = new System.Drawing.Size(111, 53);
             this.scanButton.TabIndex = 1;
             this.scanButton.Text = "Skanuj";
             this.scanButton.UseVisualStyleBackColor = true;
@@ -67,10 +67,10 @@
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(172, 293);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.exitButton.Location = new System.Drawing.Point(229, 361);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(83, 45);
+            this.exitButton.Size = new System.Drawing.Size(111, 55);
             this.exitButton.TabIndex = 2;
             this.exitButton.Text = "Wyjdź";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -78,78 +78,82 @@
             // 
             // brightnessBar
             // 
-            this.brightnessBar.Location = new System.Drawing.Point(42, 77);
-            this.brightnessBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.brightnessBar.Location = new System.Drawing.Point(56, 95);
+            this.brightnessBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.brightnessBar.Maximum = 100;
+            this.brightnessBar.Minimum = -100;
             this.brightnessBar.Name = "brightnessBar";
-            this.brightnessBar.Size = new System.Drawing.Size(214, 45);
+            this.brightnessBar.Size = new System.Drawing.Size(285, 56);
             this.brightnessBar.TabIndex = 4;
+            this.brightnessBar.TickFrequency = 20;
             this.brightnessBar.Scroll += new System.EventHandler(this.brightnessBar_Scroll);
             // 
             // contrastBar
             // 
-            this.contrastBar.Location = new System.Drawing.Point(42, 131);
-            this.contrastBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.contrastBar.LargeChange = 10;
+            this.contrastBar.Location = new System.Drawing.Point(56, 162);
+            this.contrastBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.contrastBar.Maximum = 100;
+            this.contrastBar.Minimum = -100;
             this.contrastBar.Name = "contrastBar";
-            this.contrastBar.Size = new System.Drawing.Size(214, 45);
-            this.contrastBar.TabIndex = 5;
+            this.contrastBar.Size = new System.Drawing.Size(285, 56);
+            this.contrastBar.SmallChange = 10;
+            this.contrastBar.TabIndex = 10;
+            this.contrastBar.TickFrequency = 20;
             this.contrastBar.Scroll += new System.EventHandler(this.contrastBar_Scroll);
             // 
             // scanerLabel
             // 
             this.scanerLabel.AutoSize = true;
-            this.scanerLabel.Location = new System.Drawing.Point(39, 9);
-            this.scanerLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.scanerLabel.Location = new System.Drawing.Point(52, 11);
             this.scanerLabel.Name = "scanerLabel";
-            this.scanerLabel.Size = new System.Drawing.Size(44, 13);
+            this.scanerLabel.Size = new System.Drawing.Size(57, 17);
             this.scanerLabel.TabIndex = 6;
             this.scanerLabel.Text = "Skaner:";
             // 
             // typeOfScanLabel
             // 
             this.typeOfScanLabel.AutoSize = true;
-            this.typeOfScanLabel.Location = new System.Drawing.Point(40, 179);
-            this.typeOfScanLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.typeOfScanLabel.Location = new System.Drawing.Point(53, 220);
             this.typeOfScanLabel.Name = "typeOfScanLabel";
-            this.typeOfScanLabel.Size = new System.Drawing.Size(103, 13);
+            this.typeOfScanLabel.Size = new System.Drawing.Size(134, 17);
             this.typeOfScanLabel.TabIndex = 8;
             this.typeOfScanLabel.Text = "Rodzaj skanowania:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 61);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(57, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.Size = new System.Drawing.Size(64, 17);
             this.label1.TabIndex = 9;
             this.label1.Text = "Jasność:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 109);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(57, 134);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(61, 17);
             this.label2.TabIndex = 10;
             this.label2.Text = "Kontrast";
             // 
             // typeOfScanCombo
             // 
             this.typeOfScanCombo.FormattingEnabled = true;
-            this.typeOfScanCombo.Location = new System.Drawing.Point(42, 197);
-            this.typeOfScanCombo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.typeOfScanCombo.Location = new System.Drawing.Point(56, 242);
+            this.typeOfScanCombo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.typeOfScanCombo.Name = "typeOfScanCombo";
-            this.typeOfScanCombo.Size = new System.Drawing.Size(215, 21);
+            this.typeOfScanCombo.Size = new System.Drawing.Size(285, 24);
             this.typeOfScanCombo.TabIndex = 11;
             this.typeOfScanCombo.SelectedIndexChanged += new System.EventHandler(this.typeOfScan_Box);
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(45, 243);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.saveButton.Location = new System.Drawing.Point(60, 299);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(210, 23);
+            this.saveButton.Size = new System.Drawing.Size(280, 28);
             this.saveButton.TabIndex = 12;
             this.saveButton.Text = "Zapisz do pliku";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -158,16 +162,18 @@
             // scannerListBox
             // 
             this.scannerListBox.FormattingEnabled = true;
-            this.scannerListBox.Location = new System.Drawing.Point(42, 25);
+            this.scannerListBox.ItemHeight = 16;
+            this.scannerListBox.Location = new System.Drawing.Point(56, 31);
+            this.scannerListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.scannerListBox.Name = "scannerListBox";
-            this.scannerListBox.Size = new System.Drawing.Size(212, 30);
+            this.scannerListBox.Size = new System.Drawing.Size(281, 36);
             this.scannerListBox.TabIndex = 13;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.scannerListBox);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.typeOfScanCombo);
@@ -180,7 +186,7 @@
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.scanButton);
             this.Controls.Add(this.scanImage);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Skaner/Skaner_kod/Skaner/Form1.cs
+++ Skaner/Skaner_kod/Skaner/Form1.cs
@@ -22,6 +22,32 @@ namespace Skaner
 
         int contrastLevel = 0;
         int brightnessLevel = 0;
+        int dpi = 400;
+        int colorSettting = 1;
+
+        private static void AdjustScannerSettings(IItem scannnerItem, int scanResolutionDPI, int scanStartLeftPixel, int scanStartTopPixel, int brightnessPercents, int contrastPercents, int colorMode)
+        {
+            const string WIA_SCAN_COLOR_MODE = "6146";
+            const string WIA_HORIZONTAL_SCAN_RESOLUTION_DPI = "6147";
+            const string WIA_VERTICAL_SCAN_RESOLUTION_DPI = "6148";
+            const string WIA_HORIZONTAL_SCAN_START_PIXEL = "6149";
+            const string WIA_VERTICAL_SCAN_START_PIXEL = "6150";
+            const string WIA_SCAN_BRIGHTNESS_PERCENTS = "6154";
+            const string WIA_SCAN_CONTRAST_PERCENTS = "6155";
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_RESOLUTION_DPI, scanResolutionDPI);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_RESOLUTION_DPI, scanResolutionDPI);
+            SetWIAProperty(scannnerItem.Properties, WIA_HORIZONTAL_SCAN_START_PIXEL, scanStartLeftPixel);
+            SetWIAProperty(scannnerItem.Properties, WIA_VERTICAL_SCAN_START_PIXEL, scanStartTopPixel);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_BRIGHTNESS_PERCENTS, brightnessPercents);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_CONTRAST_PERCENTS, contrastPercents);
+            SetWIAProperty(scannnerItem.Properties, WIA_SCAN_COLOR_MODE, colorMode);
+        }
+
+        private static void SetWIAProperty(IProperties properties, object propName, object propValue)
+        {
+            Property prop = properties.get_Item(ref propName);
+            prop.set_Value(ref propValue);
+        }
 
         public Form1()
         {
@@ -31,7 +57,7 @@ namespace Skaner
             typeOfScanCombo.Items.Add("Monochromatyczne");
             typeOfScanCombo.Items.Add("Czarno-białe");
             typeOfScanCombo.Items.Add("Kolorowe");
-            typeOfScanCombo.SelectedIndex = 0;
+            typeOfScanCombo.SelectedIndex = 2;
             scanImage.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
@@ -60,7 +86,21 @@ namespace Skaner
 
         private void typeOfScan_Box(object sender, EventArgs e)
         {
-
+            switch(typeOfScanCombo.SelectedIndex)
+            {
+                case 0:
+                    colorSettting = 4;
+                    break;
+                case 1:
+                    colorSettting = 2;
+                    break;
+                case 2:
+                    colorSettting = 1;
+                    break;
+                default:
+                    colorSettting = 1;
+                    break;
+            }
         }
 
         private void exitButton_Click(object sender, EventArgs e)
@@ -72,7 +112,7 @@ namespace Skaner
         {
             var selectedDevice = scannerDevices.ElementAt(scannerListBox.SelectedIndex).Connect();
             var scanner = selectedDevice.Items[1];
-
+            AdjustScannerSettings(scanner, dpi, 0, 0, brightnessLevel, contrastLevel, colorSettting);
             scannedFile = (ImageFile)scanner.Transfer(WIA.FormatID.wiaFormatJPEG);
             var imageBytes = (byte[])scannedFile.FileData.get_BinaryData();
             var ms = new MemoryStream(imageBytes);
