4b9d4e1cc07b9eb8e2685972a2434fe38ebb8571
Błażej <imnotfreeman@gmail.com>
2020-11-19 18:00:33 +0100

    add joystik, basic controls

---
+++ Joystick/Joystick/DirectInputHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Joystick
+{
+    class DirectInputHelper
+    {
+        DirectInput directInput;
+        DeviceInstance gamepadDevice;
+        public int valueX;
+        public int valueY;
+        public int valueTriggers;
+        public bool isButtonClicked;
+     
+        public DirectInputHelper()
+        {
+            valueX = 0;
+            valueY = 0;
+            valueTriggers = 0;
+            isButtonClicked = false;
+            directInput = new DirectInput();
+            gamepadDevice = null;
+        }
+
+        public string getGamepadName()
+        {
+            if (gamepadDevice == null)
+                return "Brak podlaczonego gamepada";
+
+            return gamepadDevice.InstanceName;
+        }
+
+        public void connectGamepad()
+        {
+            gamepadDevice = null;
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+            {
+                gamepadDevice = deviceInstance;
+                Console.WriteLine(deviceInstance.InstanceName);
+            }
+
+            Thread thread = new Thread(readInput);
+            thread.Start();
+        }
+
+        public void readInput()
+        {
+
+            if (gamepadDevice == null)
+                return;
+
+            SharpDX.DirectInput.Joystick joystick = new SharpDX.DirectInput.Joystick(directInput, gamepadDevice.InstanceGuid);
+
+            var allEffects = joystick.GetEffects();
+            foreach (var effectInfo in allEffects)
+                Console.WriteLine("Effect available {0}", effectInfo.Name);
+
+            // Set BufferSize in order to use buffered data.
+            joystick.Properties.BufferSize = 128;
+
+            joystick.Acquire();
+            while(true)
+            {
+                joystick.Poll();
+                var datas = joystick.GetBufferedData();
+                foreach (var state in datas)
+                {
+                    if (state.Offset.ToString().Equals("X"))
+                    {
+                        valueX = state.Value;
+                    }
+                    else if(state.Offset.ToString().Equals("Y"))
+                    {
+                        valueY = state.Value;
+                    }
+                    else if(state.Offset.ToString().Equals("Buttons0"))
+                    {
+                        switch(state.Value)
+                        {
+                            case 0: isButtonClicked = false; break;
+                            case 128: isButtonClicked = true; break;
+                        }
+                    }
+                    else if(state.Offset.ToString().Equals("Z"))
+                    {
+                        valueTriggers = state.Value;
+                    }
+                }
+            }
+        }
+    }
+}
--- Joystick/Joystick/Form1.cs
+++ Joystick/Joystick/Form1.cs
@@ -7,14 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Joystick
 {
     public partial class Form1 : Form
     {
+        DirectInputHelper helper;
+        int progressBarMaxSize = 250;
+        int maxPadValue = 66000;
+
         public Form1()
         {
             InitializeComponent();
+            helper = new DirectInputHelper();
+            label4.Size = new Size(label4.Size.Width, 0);
+            label5.Size = new Size(0, label5.Size.Height);
+            label7.Size = new Size(0, label5.Size.Height);
+        }
+
+        private void updateProgress()
+        {
+            while(true)
+            {
+
+                label4.Invoke((MethodInvoker)delegate
+                {
+                    label4.Size = new Size(label4.Size.Width, Convert.ToInt32(progressBarMaxSize * (Convert.ToDouble(helper.valueY) / maxPadValue)));
+                });
+
+                label5.Invoke((MethodInvoker)delegate
+                {
+                    label5.Size = new Size(Convert.ToInt32(progressBarMaxSize * (Convert.ToDouble(helper.valueX) / maxPadValue)), label5.Size.Height);
+                });
+
+                checkBox1.Invoke((MethodInvoker)delegate
+                {
+                    checkBox1.Checked = helper.isButtonClicked;
+                });
+
+                label7.Invoke((MethodInvoker)delegate
+                {
+                    label7.Size = new Size(Convert.ToInt32(progressBarMaxSize * (Convert.ToDouble(helper.valueTriggers) / maxPadValue)), label7.Size.Height);
+                });
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            helper.connectGamepad();
+            textBox1.Text = helper.getGamepadName();
+            Thread thread = new Thread(updateProgress);
+            thread.Start();
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
