d56541b6797150ea123e71dc2e017a1ef633af2c
Błażej <imnotfreeman@gmail.com>
2020-12-03 13:21:01 +0100

    add camera

---
+++ Kamera/CameraHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using System.Drawing;
+using AForge.Imaging.Filters;
+using Accord.Video.FFMPEG;
+namespace Kamera
+{
+    class CameraHelper
+    {
+        FilterInfoCollection videoDevices;
+        VideoCaptureDevice videoSource;
+        FilterInfo choosenDevice;
+        public Bitmap currentFrame;
+        public bool isRecording = false;
+
+        private int contrastVal;
+        private int brightnessVal;
+        private float saturationVal;
+
+        public CameraHelper()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = null;
+            choosenDevice = null;
+            currentFrame = null;
+        }
+
+        public List<String> getVideoDevicesNames()
+        {
+            List<String> deviceNames = new List<String>();
+
+            foreach(FilterInfo device in videoDevices)
+            {
+                deviceNames.Add(device.Name);
+            }
+
+            return deviceNames;
+        }
+
+        public void setCurrentDevice(string deviceName)
+        {
+            foreach(FilterInfo device in videoDevices)
+            {
+                if(device.Name.Equals(deviceName))
+                {
+                    choosenDevice = device;
+                    return;
+                }
+            }
+        }
+
+        public void startCamera()
+        {
+            videoSource = new VideoCaptureDevice(choosenDevice.MonikerString);
+            videoSource.NewFrame += videoNewFrame;
+            videoSource.Start();
+            isRecording = true;
+        }
+
+        public void stopCamera()
+        {
+            if (videoSource != null)
+                videoSource.SignalToStop();
+            isRecording = false;
+        }
+
+        private void videoNewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            //Dostosować te bazowe do kamerki
+            Threshold thresholdFilter = new Threshold(120);
+            Grayscale grayscaleFilter = new Grayscale(0.2, 0.7, 0.07);
+            BrightnessCorrection brightnessFilter = new BrightnessCorrection(brightnessVal);
+            ContrastCorrection contrastFilter = new ContrastCorrection(contrastVal);
+            SaturationCorrection saturationFilter = new SaturationCorrection(saturationVal);
+            bitmap = brightnessFilter.Apply((Bitmap)bitmap.Clone());
+            bitmap = contrastFilter.Apply((Bitmap)bitmap.Clone());
+            bitmap = saturationFilter.Apply((Bitmap)bitmap.Clone());
+
+
+            currentFrame = (Bitmap)bitmap.Clone();
+        }
+
+        public void recordVideo()
+        {
+            VideoFileWriter fileWriter = new VideoFileWriter();
+            fileWriter.Open("test.avi", currentFrame.Width, currentFrame.Height, 60, VideoCodec.Default, 10000);
+
+            while(isRecording)
+            {
+                fileWriter.WriteVideoFrame((Bitmap)currentFrame.Clone());
+            }
+
+            fileWriter.Close();
+        }
+    }
+
+
+
+}
---
+++ Kamera/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Kamera
+{
+    public partial class Form1 : Form
+    {
+        CameraHelper helper;
+
+        public Form1()
+        {
+            InitializeComponent();
+            helper = new CameraHelper();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<String> names = helper.getVideoDevicesNames();
+            listBox1.Items.AddRange(names.ToArray());
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            helper.setCurrentDevice(listBox1.SelectedItem.ToString());
+
+            Thread thread = new Thread(updatePictureBox);
+            thread.Start();
+        }
+
+        private void updatePictureBox()
+        {
+            helper.startCamera();
+
+            while(helper.isRecording)
+            {
+                pictureBox1.Invoke((MethodInvoker)delegate
+                {
+                    if(helper.currentFrame != null)
+                        pictureBox1.Image = (Bitmap)helper.currentFrame.Clone();
+                });
+            }
+
+            pictureBox1.Image = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            helper.stopCamera();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(helper.recordVideo);
+            thread.Start();
+        }
+    }
+}
